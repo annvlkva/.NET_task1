75ee238fdfbcebf9d2e38b0db4dde7072d74356c
annvlkva <anservol@gmail.com>
2021-02-27 12:52:06 +0300

    create

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace task1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
