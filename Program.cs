8080f888367b357f8dafeb84e67f31d74d004926
annvlkva <anservol@gmail.com>
2021-03-09 17:09:44 +0300

    add binary tree

--- Program.cs
+++ Program.cs
@@ -6,12 +6,38 @@ namespace task1
     {
         static void Main(string[] args)
         {
+            // 1
+            /*
             LinkedList<string> linkedList = new LinkedList<string> {"one", "two", "three", "four"};
             linkedList.Show();
             linkedList.Remove("four");
             linkedList.Show();
             linkedList.Reverse();
             linkedList.Show();
+            */
+            
+            //2
+            var binaryTree = new BinaryTree<int>();
+
+            binaryTree.Add(8);
+            binaryTree.Add(3);
+            binaryTree.Add(10);
+            binaryTree.Add(1);
+            binaryTree.Add(6);
+            binaryTree.Add(4);
+            binaryTree.Add(7);
+            binaryTree.Add(14);
+            binaryTree.Add(16);
+
+            binaryTree.PrintTree();
+
+            Console.WriteLine(new string('-', 40));
+            binaryTree.Remove(3);
+            binaryTree.PrintTree();
+
+            Console.WriteLine(new string('-', 40));
+            binaryTree.Remove(8);
+            binaryTree.PrintTree();
         }
     }
 }
