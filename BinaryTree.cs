dcb3bb18ef0532be24b78c48c1e68cb5e69ee8f8
annvlkva <anservol@gmail.com>
2021-03-09 17:11:07 +0300

    add binary tree

---
+++ BinaryTree.cs
@@ -0,0 +1,176 @@
+﻿using System;
+
+namespace task1
+{
+    public enum Side
+    {
+        Left,
+        Right
+    }
+    
+    public class BinaryTreeNode
+    {
+        public BinaryTreeNode(int data)
+        {
+            Data = data;
+        }
+
+        public int Data { get; set; }
+
+        public BinaryTreeNode LeftNode { get; set; }
+
+        public BinaryTreeNode RightNode { get; set; }
+
+        public BinaryTreeNode ParentNode { get; set; }
+
+        public Side? NodeSide =>
+            ParentNode == null
+            ? (Side?)null
+            : ParentNode.LeftNode == this
+                ? Side.Left
+                : Side.Right;
+
+        public override string ToString() => Data.ToString();
+    }
+    public class BinaryTree<T> where T : IComparable
+    {
+        public BinaryTreeNode RootNode { get; set; }
+
+        public BinaryTreeNode Add(BinaryTreeNode node, BinaryTreeNode currentNode = null)
+        {
+            if (RootNode == null)
+            {
+                node.ParentNode = null;
+                return RootNode = node;
+            }
+
+            currentNode = currentNode ?? RootNode;
+            node.ParentNode = currentNode;
+            int result;
+            return (result = node.Data.CompareTo(currentNode.Data)) == 0
+                ? currentNode
+                : result < 0
+                    ? currentNode.LeftNode == null
+                        ? (currentNode.LeftNode = node)
+                        : Add(node, currentNode.LeftNode)
+                    : currentNode.RightNode == null
+                        ? (currentNode.RightNode = node)
+                        : Add(node, currentNode.RightNode);
+        }
+
+        public BinaryTreeNode Add(int data)
+        {
+            return Add(new BinaryTreeNode(data));
+        }
+
+        public BinaryTreeNode FindNode(T data, BinaryTreeNode startWithNode = null)
+        {
+            startWithNode = startWithNode ?? RootNode;
+            int result;
+            return (result = data.CompareTo(startWithNode.Data)) == 0
+                ? startWithNode
+                : result < 0
+                    ? startWithNode.LeftNode == null
+                        ? null
+                        : FindNode(data, startWithNode.LeftNode)
+                    : startWithNode.RightNode == null
+                        ? null
+                        : FindNode(data, startWithNode.RightNode);
+        }
+
+        public void Remove(BinaryTreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            var currentNodeSide = node.NodeSide;
+            if (node.LeftNode == null && node.RightNode == null)
+            {
+                if (currentNodeSide == Side.Left)
+                {
+                    node.ParentNode.LeftNode = null;
+                }
+                else
+                {
+                    node.ParentNode.RightNode = null;
+                }
+            }
+            else if (node.LeftNode == null)
+            {
+                if (currentNodeSide == Side.Left)
+                {
+                    node.ParentNode.LeftNode = node.RightNode;
+                }
+                else
+                {
+                    node.ParentNode.RightNode = node.RightNode;
+                }
+
+                node.RightNode.ParentNode = node.ParentNode;
+            }
+            else if (node.RightNode == null)
+            {
+                if (currentNodeSide == Side.Left)
+                {
+                    node.ParentNode.LeftNode = node.LeftNode;
+                }
+                else
+                {
+                    node.ParentNode.RightNode = node.LeftNode;
+                }
+
+                node.LeftNode.ParentNode = node.ParentNode;
+            }
+            else
+            {
+                switch (currentNodeSide)
+                {
+                    case Side.Left:
+                        node.ParentNode.LeftNode = node.RightNode;
+                        node.RightNode.ParentNode = node.ParentNode;
+                        Add(node.LeftNode, node.RightNode);
+                        break;
+                    case Side.Right:
+                        node.ParentNode.RightNode = node.RightNode;
+                        node.RightNode.ParentNode = node.ParentNode;
+                        Add(node.LeftNode, node.RightNode);
+                        break;
+                    default:
+                        var bufLeft = node.LeftNode;
+                        var bufRightLeft = node.RightNode.LeftNode;
+                        var bufRightRight = node.RightNode.RightNode;
+                        node.Data = node.RightNode.Data;
+                        node.RightNode = bufRightRight;
+                        node.LeftNode = bufRightLeft;
+                        Add(bufLeft, node);
+                        break;
+                }
+            }
+        }
+
+        public void Remove(T data)
+        {
+            var foundNode = FindNode(data);
+            Remove(foundNode);
+        }
+
+        public void PrintTree()
+        {
+            PrintTree(RootNode);
+        }
+
+        private void PrintTree(BinaryTreeNode startNode, string indent = "", Side? side = null)
+        {
+            if (startNode != null)
+            {
+                var nodeSide = side == null ? "+" : side == Side.Left ? "L" : "R";
+                Console.WriteLine($"{indent} [{nodeSide}]- {startNode.Data}");
+                indent += new string(' ', 3);
+                PrintTree(startNode.LeftNode, indent, Side.Left);
+                PrintTree(startNode.RightNode, indent, Side.Right);
+            }
+        }
+    }
+}
